239140954f77f3d971e75c27f9245933f563daec
root <root@localhost>
2021-04-18 14:31:20 +0200

    add HanderError class

---
+++ src/handler-error.ts
@@ -0,0 +1,15 @@
+export class HandlerError<T extends object = any> extends Error {
+    constructor(
+        public readonly data: T,
+        public readonly handler: Function,
+        ...params: any[]
+    ) {
+        super(...params);
+
+        this.name = 'HandlerError';
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, HandlerError)
+        }
+    }
+}

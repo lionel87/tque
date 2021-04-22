c61153d800f2efddaa6d92e87d92e6483d31748b
root <root@localhost>
2021-04-22 07:56:13 +0200

    improve type checks file

--- tests/type-checks-by-hand.ts
+++ tests/type-checks-by-hand.ts
@@ -10,19 +10,28 @@ import { create, createWithContext, series, parallel, branch } from '../lib/inde
     const api: TestApi = { testFn() { return 1; } };
 
     // this should be ok
-    const cf1 = create<TestData>(series(series(function(d){ d.testKey; })));
-    const cf2 = createWithContext<TestData, TestApi>(api, series(function(d){ this.testFn(); d.testKey; }));
-    const cf3 = createWithContext<TestData, TestApi>(api, series(branch(parallel(series(function(d){ this.testFn(); d.testKey; })))));
+    const cf1 = create<TestData>(series(series(function (d) { d.testKey; })));
+    const cf2 = createWithContext<TestData, TestApi>(api, series(function (d) { this.testFn(); d.testKey; }));
+    const cf3 = createWithContext<TestData, TestApi>(api, series(branch(parallel(series(function (d) { this.testFn(); d.testKey; })))));
 
     cf1({ testKey: 'abc' });
     cf2({ testKey: 'abc' });
     cf3({ testKey: 'abc' });
 
+    series(function ({ a, b }) { }, function ({ c }) {})({ d: 1 });
+
+    type ABCD = { a: string; b: number; c: boolean; d: number };
+    series<Partial<ABCD>>(function ({ a, b }) { }, function ({ c }) {})({ d: 1 });
+
     // should show errors
-    const cf4 = createWithContext<TestData, TestApi>({}, series(function(d){ this.testFn(); d.testKey; }));
-    const cf5 = createWithContext<TestData, TestApi>({}, series(function(d){ this.notExistingFn(); d.notExistingKey; }));
+    const cf4 = createWithContext<TestData, TestApi>({}, series(function (d) { this.testFn(); d.testKey; }));
+    const cf5 = createWithContext<TestData, TestApi>({}, series(function (d) { this.notExistingFn(); d.notExistingKey; }));
 
     cf4({ x: 1 });
     cf5({ x: 1 });
+    
+    series<Partial<{ x: string }>>(function ({ a, b }) { }, function ({ c }) {})({ d: 1 });
+    series<ABCD>(function ({ a, y }) { }, function ({ z }) {})({ d: 1 });
+
 
 })();
